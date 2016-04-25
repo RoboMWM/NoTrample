e75976c3b579765933a7ae5cb1dc77a63452be62
RoboMWM <robomwm@users.noreply.github.com>
2016-04-24 22:47:00 -0700

    1st

---
+++ src/main/java/me/robomwm/NoTrample/NoTrample.java
@@ -0,0 +1,30 @@
+package me.robomwm.NoTrample;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.player.PlayerAnimationEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Robo on 4/24/2016.
+ */
+public class NoTrample extends JavaPlugin implements Listener
+{
+    @Override
+    public void onEnable()
+    {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    void onTrampleBlock(EntityChangeBlockEvent event)
+    {
+        Material newBlock = event.getTo();
+        Material oldBlock = event.getBlock().getType();
+        if (newBlock == Material.DIRT && oldBlock == Material.SOIL)
+            event.setCancelled(true);
+    }
+}
